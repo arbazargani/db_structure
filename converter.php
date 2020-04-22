57912d90bada762978a5b4bf01d3fca7528f09a4
علی رضا بازرگانی <a.bazargani@smt.news>
2020-04-22 19:43:29 +0430

    first test, added ignores, ...

---
+++ converter.php
@@ -0,0 +1,99 @@
+<?php
+    /*
+     * This script is for converting joomla database to wordpress database.
+     * note: this is beta and won't work for your purpose i think!
+     * @author: alireza bazargani
+     *
+     * let's go so ...
+     */
+
+    $env = require 'env.php';
+
+    /**
+     * @param $env
+     * @return false|mysqli
+     * to make a MYSQLI Procedural connection.
+     */
+    function make_handle($env, $side = 'joomla') {
+        $servername = $env['db_host'];
+        $username = $env['db_username'];
+        $password = $env['db_password'];
+        $db_name = ($side == 'joomla') ? $env['joomla_db_name'] : $env['wordpress_db_name'];
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $db_name);
+
+        // Check connection
+        if (!$conn) {
+            echo "<pre>function: make_handle($side) -> <span style='color: orangered; font-weight: bold;'>";
+            echo "Connection failed: " . mysqli_connect_error();
+            echo "</span></pre>";
+            die();
+
+        }
+
+
+        echo "<pre>function: make_handle($side) -> <span style='color: #00cc00; font-weight: bold;'>Connected successfully.</span></pre>";
+        return $conn;
+    }
+
+
+    function move_articles($env) {
+        $jconn = make_handle($env);
+        $wconn = make_handle($env, 'wordpress');
+
+        $jsql = "
+                SELECT 
+                `id`,
+                `title`,
+                `alias` AS 'slug',
+                CONCAT_WS('-', `id`, `alias`) AS 'joomla_slug',
+                `introtext` AS 'lead',
+                `metadesc` AS 'meta_description',
+                `metakey` AS 'meta_keywords',
+                `fulltext` AS 'content',
+                `catid` AS 'category',
+                `created_by` AS 'author',
+                `created` AS 'created_at',
+                `hits` AS 'views'
+                FROM `rokh1_content` ORDER BY `id` ASC;
+        ";
+        $result = mysqli_query($jconn, $jsql);
+
+        if (mysqli_num_rows($result) > 0) {
+
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                $id = $row['id'];
+                $post_author = $row['author'];
+                $post_date = $row['created_at'];
+                $post_date_gmt = $row['created_at'];
+                $post_content =  $row['content'];
+                $post_title =  $row['title'];
+                $post_excerpt = $row['lead'];
+                $post_status = 'publish';
+                $comment_status = 'open';
+                $post_name = $row['joomla_slug'];
+                $guid = $env['wordpress_domain'] . "?p=$id";
+                $wsql = "INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES
+                                                ('$id', '$post_author', '$post_date', '$post_date_gmt', '$post_content', '$post_title', '$post_excerpt', '$post_status', 'open', 'open', '', '$post_name', '', '', '$post_date', '$post_date', '', '', '$guid', '0', 'post', '', '0');";
+//                echo $wsql;
+                echo '<pre>';
+//                print_r($row);
+//                echo '</pre><hr/>';
+
+                if (mysqli_query($wconn, $wsql)) {
+                    echo "<pre>New record created successfully</pre>";
+                } else {
+                    echo "Error: " . $wsql . "<br>" . mysqli_error($wconn);
+                }
+                echo '</pre>';
+            }
+        } else {
+            echo "<pre>function: make_handle(joomla) -> <span style='color: #f5a200; font-weight: bold;'>0 results fetched from joomla database.</span></pre>";
+        }
+
+        mysqli_close($jconn);
+    }
+
+    move_articles($env);
---
+++ env.php
@@ -0,0 +1,27 @@
+<?php
+    /*
+     * the application environments will define here.
+     */
+
+    return [
+        /*
+         * public environments
+         */
+        'db_host' => 'localhost',
+        'db_port' => '',
+        'db_username' => 'root',
+        'db_password' => '',
+        /*
+         * wordpress domain
+         */
+        'wordpress_domain' => 'http://localhost:8080/pure-wp/',
+
+        
+
+
+        /*
+         * change the database names, it's better to use a clean wordpress database.
+         */
+        'joomla_db_name' => 'khodro',
+        'wordpress_db_name' => 'pure-wp',
+    ];
